8038b806b3179ec5895f7ee7aaa5684b9af8e7e7
Mathew Al-Frenn <malfr102@uottawa.ca>
2024-10-12 13:07:55 -0400

    Changing some of the classes

--- EAMS/app/src/users/Attendee.java
+++ EAMS/app/src/users/Attendee.java
@@ -1,23 +1,32 @@
 /**
  * The purpose of this class is to create a template to create an Attendee instance
- * 
+ *
  * @author Sijun Liu
  */
-public class Attendee extends PersonAbstract{
-    
-   /**
-    * Constructor that creates an Attendee
-    * 
-    * @param firstName, lastName, email, password, phoneNum, address
-    */
-    public Attendee(String firstName, String lastName, String email, 
-        String password, long phoneNum, String address){
-            super(firstName, lastName, email, password, phoneNum, address);
+public class Attendee extends User{
+
+    /**
+     * Constructor that creates an Attendee
+     *
+     * @param firstName, lastName, email, password, phoneNum, address
+     */
+    public Attendee(String firstName, String lastName, String email,
+                    String password, long phoneNum, String address){
+        super(firstName, lastName, email, password, phoneNum, address);
+    }
+
+    /**
+     * Method to return the role of the user
+     *
+     * @return String, the role of the user
+     */
+    public String getRole(){
+        return "Attendee";
     }
 
     /**
      * toString method to create a logical representation of the object
-     * 
+     *
      * @return logical representation of the object
      */
     @Override
