80929d42d2c3e998867887a469fd977be0ac2af0
Bradleyh212 <bdesm052@uottawa.ca>
2024-10-12 17:01:39 -0400

    finished database

--- EAMS/app/src/users/DataBaseHelper.java
+++ EAMS/app/src/users/DataBaseHelper.java
@@ -1,28 +1,19 @@
-package com.example.booklibrarysqlite;
-
 import static android.accounts.AccountManager.KEY_PASSWORD;
+
 
 import android.content.ContentValues;
 import android.content.Context;
-import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class DatabaseHelper extends SQLiteOpenHelper {
 
-    // Database Version
     private static final int DATABASE_VERSION = 1;
 
-    // Database Name
     private static final String DATABASE_NAME = "Users";
 
-    // Users table name
     private static final String TABLE_USERS = "Users";
 
-    // Table columns
     private static final String KEY_EMAIL = "email";
     private static final String KEY_FIRST_NAME = "first_name";
     private static final String KEY_LAST_NAME = "last_name";
@@ -37,6 +28,8 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
+        // SQL statement for creating the Users table
+        String CREATE_USERS_TABLE = "CREATE TABLE IF NOT EXISTS " + TABLE_USERS + " ("
                 + KEY_FIRST_NAME + " TEXT,"
                 + KEY_LAST_NAME + " TEXT,"
                 + KEY_EMAIL + " TEXT PRIMARY KEY,"
@@ -51,63 +44,9 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         // Drop older table if existed
         db.execSQL("DROP TABLE IF EXISTS " + TABLE_USERS);
+
         // Create tables again
         onCreate(db);
-    }
-
-    public List<User> findUsers(String emailFilter, String firstNameFilter, String lastNameFilter, String passwordFilter, Integer numberFilter, String addressFilter, String roleFilter) {
-        List<User> result = new ArrayList<>();
-        String query = "SELECT * FROM " + TABLE_USERS + " WHERE 1=1 ";
-        List<String> args = new ArrayList<>();
-
-        if (!emailFilter.isEmpty()) {
-            query += " AND " + KEY_EMAIL + " LIKE ?";
-            args.add("%" + emailFilter + "%");
-        }
-        if (!firstNameFilter.isEmpty()) {
-            query += " AND " + KEY_FIRST_NAME + " LIKE ?";
-            args.add("%" + firstNameFilter + "%");
-        }
-        if (!lastNameFilter.isEmpty()) {
-            query += " AND " + KEY_LAST_NAME + " = ?";
-            args.add(lastNameFilter);
-        }
-        if (!passwordFilter.isEmpty()) {
-            query += " AND " + KEY_PASS + " = ?";
-            args.add(passwordFilter);
-        }
-        if (numberFilter != null) {
-            query += " AND " + KEY_NUMBER + " = ?";
-            args.add(String.valueOf(numberFilter));
-        }
-        if (!addressFilter.isEmpty()) {
-            query += " AND " + KEY_ADDRESS + " = ?";
-            args.add(addressFilter);
-        }
-        if (!roleFilter.isEmpty()) {
-            query += " AND " + KEY_ROLE + " = ?";
-            args.add(roleFilter);
-        }
-
-        SQLiteDatabase db = this.getReadableDatabase();
-        Cursor cursor = db.rawQuery(query, args.toArray(new String[0]));
-
-        if (cursor.moveToFirst()) {
-            do {
-                User user = new User(
-                        cursor.getString(0),  // First Name
-                        cursor.getString(1),  // Last Name
-                        cursor.getString(2),  // Email
-                        cursor.getString(3),  // Password
-                        cursor.getInt(4),     // Phone Number
-                        cursor.getString(5),  // Address
-                        cursor.getString(6)   // Role
-                );
-                result.add(user);
-            } while (cursor.moveToNext());
-        }
-        cursor.close();
-        return result;
     }
 
     public void addUser(String firstName, String lastName, String email, String password, Integer phoneNum, String address, String role) {
@@ -122,7 +61,8 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         values.put(KEY_ADDRESS, address);
         values.put(KEY_ROLE, role);
 
+        // Inserting Row
         db.insert(TABLE_USERS, null, values);
-        db.close();
+        db.close(); // Closing database connection
     }
 }
