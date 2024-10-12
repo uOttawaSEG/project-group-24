7fcedec8ec04fdfbc0f327a2c8175e4e4d983206
Mathew Al-Frenn <malfr102@uottawa.ca>
2024-10-12 13:58:38 -0400

    starting to work on the whole implementation

--- EAMS/app/src/main/java/com/example/eams/MainActivity.java
+++ EAMS/app/src/main/java/com/example/eams/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.eams;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +11,41 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    //UI component
+    private EditText username, password;
+    private Button register, login;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        initializeViews();
+        setClickListeners();
     }
+    private void initializeViews() {
+        username = findViewById(R.id.username);
+        password = findViewById(R.id.password);
+        register = findViewById(R.id.Register);
+        login = findViewById(R.id.submitButton);
+
+    }
+
+    private void setClickListeners() {
+        register.setOnClickListener(v -> RegisterUser());
+        login.setOnClickListener(v -> LoginUser());
+
+    }
+
+    private void LoginUser() {
+        System.out.println("hello");
+    }
+
+    private void RegisterUser() {
+        
+    }
+
+
 }
