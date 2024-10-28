579a861fa1365365c05ccb530e1410bc55537f7e
Davy22 <shadavid019@gmail.com>
2024-10-28 15:36:51 -0400

    Merge remote-tracking branch 'origin/main'

---
+++ EAMS/app/src/main/java/com/example/eams/RejectedRequests.java
@@ -0,0 +1,11 @@
+package com.example.eams;
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class RejectedRequests extends AppCompatActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rejects); // Ensure this layout exists in res/layout
+    }
+}
