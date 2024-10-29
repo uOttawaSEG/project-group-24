4cee5d7501a22d9842068fa214ec553bfd45aade
Davy22 <shadavid019@gmail.com>
2024-10-29 16:29:43 -0400

    user registration page

---
+++ EAMS/app/src/main/java/com/example/eams/UserRegistration.java
@@ -0,0 +1,50 @@
+package com.example.eams;
+
+public class UserRegistration {
+    private final String firstName;
+    private final String lastName;
+    private final String email;
+    private final String password;
+    private final String phone;
+    private final String address;
+    private final String role;
+
+    public UserRegistration(String firstName, String lastName, String email, String password, String phone, String address, String role) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+        this.phone = phone;
+        this.address = address;
+        this.role = role;
+    }
+
+    // Getters for each field
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getRole() {
+        return role;
+    }
+}
