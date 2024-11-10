3d52adf25e3da45b24d06c10470d8031fb51fd15
Mathew Al-Frenn <malfr102@uottawa.ca>
2024-11-10 17:54:19 -0500

    addded option to delete event

--- EAMS/app/src/main/java/com/example/eams/ViewEventActivity.java
+++ EAMS/app/src/main/java/com/example/eams/ViewEventActivity.java
@@ -1,14 +1,20 @@
 package com.example.eams;
 
+import android.content.DialogInterface;
 import android.os.Build;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.Toast;
 
 import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -72,5 +78,39 @@ public class ViewEventActivity extends AppCompatActivity {
         // Set past events to the ListView
         ArrayAdapter<Event> pastAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, pastEvents);
         past.setAdapter(pastAdapter);
+
+        // Set long click listeners on ListView items to show options (Delete or Go Back)
+        upcoming.setOnItemLongClickListener((adapterView, view, position, id) -> {
+            Event event = upcomingEvents.get(position);
+            showDeleteOrBackDialog(event);
+            return true;
+        });
+
+        past.setOnItemLongClickListener((adapterView, view, position, id) -> {
+            Event event = pastEvents.get(position);
+            showDeleteOrBackDialog(event);
+            return true;
+        });
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    private void showDeleteOrBackDialog(final Event event) {
+        AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(this);
+        dialogBuilder.setMessage("What would you like to do?")
+                .setPositiveButton("Delete", (dialog, id) -> deleteEvent(event))
+                .setNegativeButton("Go Back", (dialog, id) -> dialog.dismiss())
+                .create()
+                .show();
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    private void deleteEvent(Event event) {
+        boolean isDeleted = dbHelper.deleteEvent(event.getEventId());
+        if (isDeleted) {
+            Toast.makeText(this, "Event deleted successfully!", Toast.LENGTH_SHORT).show();
+            loadEvents();  // Refresh the events list
+        } else {
+            Toast.makeText(this, "Failed to delete event.", Toast.LENGTH_SHORT).show();
+        }
     }
 }
